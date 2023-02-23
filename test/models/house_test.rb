2686e6a3bc380f4e8c8bc3746fbd9b4e81bf85bc
Jerome Tan <jerome.tan@tuta.io>
2023-02-23 14:21:41 +0100

    created House model tests

---
+++ test/models/house_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+
+class HouseTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    @house = House.new(name: "Example house")
+  end
+
+  test "should be valid" do
+    assert @house.valid?
+  end
+
+  test "name should be present" do
+    @house.name = "     "
+    assert_not @house.valid?
+  end
+
+  test "name should not be too long" do
+    @house.name = "a" * 51
+    assert_not @user.valid?
+  end
+end
