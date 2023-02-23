1dcbdd494982bcea546ef18d6144ce8042e399e3
Jerome Tan <jerome.tan@tuta.io>
2023-02-23 14:27:52 +0100

    generated Tribe model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_23_125604) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_23_132635) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_23_125604) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tribes", force: :cascade do |t|
+    t.integer "keys"
+    t.string "color"
+    t.float "shareholding"
+    t.bigint "house_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["house_id"], name: "index_tribes_on_house_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +42,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_23_125604) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tribes", "houses"
 end
