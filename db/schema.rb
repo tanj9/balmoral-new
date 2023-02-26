de142b9eb0d5c4a75467a65c8b92d9bc335beea3
Jerome Tan <jerome.tan@tuta.io>
2023-02-26 19:53:34 +0100

    generated Spending model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_23_132635) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_26_185227) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_23_132635) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "spendings", force: :cascade do |t|
+    t.float "amount"
+    t.date "date"
+    t.string "category"
+    t.text "details"
+    t.bigint "tribe_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tribe_id"], name: "index_spendings_on_tribe_id"
   end
 
   create_table "tribes", force: :cascade do |t|
@@ -42,5 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_23_132635) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "spendings", "tribes"
   add_foreign_key "tribes", "houses"
 end
