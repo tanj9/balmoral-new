bf48ac7df4c03cf8d0c3a7eb8df3cd023d3146fb
Jerome Tan <jerome.tan@tuta.io>
2023-02-26 20:19:49 +0100

    created Spending model tests

--- app/models/tribe.rb
+++ app/models/tribe.rb
@@ -1,5 +1,6 @@
 class Tribe < ApplicationRecord
   belongs_to :house
+  has_many :spendings
 
   validates :keys, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: 1000 }
   validates :color, presence: true, length: { maximum: 50 }
