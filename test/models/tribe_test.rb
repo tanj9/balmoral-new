af4d423145075df7b2bd8d5bf81cf52e3f4fa93a
Jerome Tan <jerome.tan@tuta.io>
2023-02-26 19:30:47 +0100

    created Tribe model tests

--- test/models/tribe_test.rb
+++ test/models/tribe_test.rb
@@ -1,7 +1,62 @@
 require "test_helper"
 
 class TribeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @house = houses(:one)
+    @tribe = @house.tribes.build(keys: 10, color: "blue", shareholding: 1)
+  end
+
+  test "should be valid" do
+    assert @tribe.valid?
+  end
+
+  test "keys should be present" do
+    @tribe.keys = nil
+    assert_not @tribe.valid?
+  end
+
+  test "keys should be an integer" do
+    @tribe.keys = 0.5
+    assert_not @tribe.valid?
+  end
+
+  test "keys should be greater or equal to zero" do
+    @tribe.keys = -1
+    assert_not @tribe.valid?
+  end
+
+  test "color should be present" do
+    @tribe.color = "     "
+    assert_not @tribe.valid?
+  end
+
+  test "color should not be less than 50 characters" do
+    @tribe.color = "a" * 51
+    assert_not @tribe.valid?
+  end
+
+  test "shareholding should be present" do
+    @tribe.shareholding = nil
+    assert_not @tribe.valid?
+  end
+
+  test "shareholding should be greater than zero" do
+    @tribe.shareholding = -0.00001
+    assert_not @tribe.valid?
+  end
+
+  test "shareholding should be different from zero" do
+    @tribe.shareholding = 0
+    assert_not @tribe.valid?
+  end
+
+  test "shareholding should be less than or equal to 1" do
+    @tribe.shareholding = 1.0001
+    assert_not @tribe.valid?
+  end
+
+  test "house should be present" do
+    @tribe.house_id = nil
+    assert_not @tribe.valid?
+  end
 end
