2421c713c371adf6c1e9ca058c3c40f107fbef0a
Jerome Tan <jerome.tan@tuta.io>
2023-02-26 20:53:48 +0100

    added 4 columns to users table (including reference to tribe)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_26_185227) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_26_195231) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,10 +49,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_26_185227) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "role"
+    t.bigint "tribe_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["tribe_id"], name: "index_users_on_tribe_id"
   end
 
   add_foreign_key "spendings", "tribes"
   add_foreign_key "tribes", "houses"
+  add_foreign_key "users", "tribes"
 end
