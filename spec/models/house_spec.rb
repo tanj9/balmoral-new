d6687fc0b5ebaf93d09f38cf566748dcfb47c24b
Jerome Tan <jerome.tan@tuta.io>
2023-04-14 17:49:44 +0200

    installed shoulda-matchers gem and refactored spec on House model

--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -3,17 +3,18 @@ require 'rails_helper'
 RSpec.describe House, type: :model do
   subject { House.new(name: "kermaria") }
 
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
+  describe 'associations' do
+    it { should have_many(:tribes) }
+    it { should have_many(:platforms) }
+    it { should have_many(:users) }
+    it { should have_many(:bookings) }
   end
 
-  it 'should have a name' do
-    subject.name = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'should have a name which is not too long' do
-    subject.name = "a" * 51
-    expect(subject).to_not be_valid
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_most(50) }
   end
 end
