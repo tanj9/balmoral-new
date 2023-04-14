5c123ecc3b3d8ee6be5aefdf10a8eea7301a6572
Jerome Tan <jerome.tan@tuta.io>
2023-04-14 12:08:44 +0200

    added spec for House model

---
+++ spec/models/house_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe House, type: :model do
+  subject { House.new(name: "kermaria") }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have a name which is not too long' do
+    subject.name = "a" * 51
+    expect(subject).to_not be_valid
+  end
+end
