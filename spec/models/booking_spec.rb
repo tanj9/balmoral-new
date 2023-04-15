55834fbfe94c86f1d27b1bc572ff71aa5583f2c6
Jerome Tan <jerome.tan@tuta.io>
2023-04-15 15:39:46 +0200

    refactored (bis) spec for Booking model

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -28,38 +28,17 @@ RSpec.describe Booking, type: :model do
     )
   end
 
-  let(:user_booking) do
+  subject do
     Booking.new(
       start_date: Date.today,
       end_date: Date.today + 1,
       key_price: 1,
       status: "pending",
       user:,
-      house:
-    )
-  end
-
-  let(:platform_booking) do
-    Booking.new(
-      start_date: Date.today + 1,
-      end_date: Date.today + 2,
-      key_price: 1,
-      status: "pending",
       platform:,
       house:
     )
   end
-
-  subject {
-    Booking.new(
-      start_date: Date.today,
-      end_date: Date.today + 1,
-      key_price: 1,
-      status: "pending",
-      user:,
-      house:
-    )
-  }
 
   describe 'associations' do
     it { should belong_to(:house) }
@@ -76,12 +55,16 @@ RSpec.describe Booking, type: :model do
   end
 
   describe 'validations' do
-    describe 'valid with valid attributes' do
-      it 'booking from platform should be valid with valid attributes' do
-        expect(platform_booking).to be_valid
+    context 'when booking is from platform' do
+      before { allow(subject).to receive(:user).and_return(nil) }
+      it 'should be valid with valid attributes' do
+        expect(subject).to be_valid
       end
-      it 'booking from user should be valid with valid attributes' do
-        expect(user_booking).to be_valid
+    end
+    context 'when booking is from user' do
+      before { allow(subject).to receive(:platform).and_return(nil) }
+      it 'should be valid with valid attributes' do
+        expect(subject).to be_valid
       end
     end
 
@@ -100,16 +83,16 @@ RSpec.describe Booking, type: :model do
     describe 'start_date' do
       it { should validate_presence_of(:start_date) }
       it 'start date should be a date' do
-        user_booking.start_date = "a given date"
-        expect(user_booking).to_not be_valid
+        subject.start_date = "a given date"
+        expect(subject).to_not be_valid
       end
     end
 
     describe 'end_date' do
       it { should validate_presence_of(:end_date) }
       it 'end date should be a date' do
-        user_booking.end_date = "another given date"
-        expect(user_booking).to_not be_valid
+        subject.end_date = "another given date"
+        expect(subject).to_not be_valid
       end
     end
   end
